32c9c9149c1896def6e9348b981a0ff8c1789f97
kieroc44 <kierolleskamel@gmail.com>
2025-07-06 15:26:22 +0300

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using Common;
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Class Libraries
+            Product product = new Product();
+            product.Id = 10;
+            product.Name = "Test";
+            product.Price = 1000;
+            product.Quantity = 1;
+            #endregion
+        }
+    }
+}
