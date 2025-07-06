4f6139e769095f551adca48bf0e8938f3d3b9c42
kieroc44 <kierolleskamel@gmail.com>
2025-07-06 15:29:15 +0300

    Video01

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -6,12 +6,13 @@ namespace Demo
         static void Main(string[] args)
         {
             #region Class Libraries
-            Product product = new Product();
-            product.Id = 10;
-            product.Name = "Test";
-            product.Price = 1000;
-            product.Quantity = 1;
+            //Product product = new Product();
+            //product.Id = 10;
+            //product.Name = "Test";
+            //product.Price = 1000;
+            //product.Quantity = 1;
             #endregion
+
         }
     }
 }
