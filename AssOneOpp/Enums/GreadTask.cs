87bb926e0e260fb5ff02529a08f5d2242d4d5c9f
kieroc44 <kierolleskamel@gmail.com>
2025-07-07 21:17:54 +0300

    Task

---
+++ AssOneOpp/Enums/GreadTask.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace AssOneOpp.Enums
+{
+    internal enum GreadTask
+    {
+        A =1,
+        B,
+        C,
+        D,
+        F
+    }
+}
--- AssOneOpp/Program.cs
+++ AssOneOpp/Program.cs
@@ -1,9 +1,84 @@
-﻿namespace AssOneOpp
+﻿using AssOneOpp.Structs;
+
+namespace AssOneOpp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+
+            #region Task
+            StructTask student;
+
+            Console.Write("Enter student name: ");
+            student.Name = Console.ReadLine();
+
+            Console.Write("Enter student grade (A, B, C, D, F): ");
+            string gradeInput = Console.ReadLine();
+
+
+            if (gradeInput == "A" || gradeInput == "B" || gradeInput == "C" || gradeInput == "D" || gradeInput == "F")
+            {
+                Grade parsedGrade = (Grade)Enum.Parse(typeof(Grade), gradeInput);
+                student.Grade = parsedGrade;
+
+                Console.WriteLine($"\nStudent: {student.Name}");
+                Console.WriteLine($"Grade: {student.Grade}");
+
+                switch (gradeInput) 
+                {
+                    case "A":
+                    case "B":
+                        Console.WriteLine("Message: Excellent");
+                        break;
+                    case "C":
+                        Console.WriteLine("Message: Good job");
+                        break;
+                    case "D":
+                        Console.WriteLine("Message: Needs improvement");
+                        break;
+                    case "F":
+                        Console.WriteLine("Message: Failed. Try again");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid grade entered.");
+            }
+            #endregion
+
+
+            #region 1.Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
+
+            #endregion
+
+            #region 2.Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+
+            #endregion
+
+            #region 3.Create an enum called "Season" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
+
+            #endregion
+
+            #region 4.Assign the following Permissions (Read, write, Delete, Execute) in a form of Enum. ⮚Create Variable from previous Enum to Add and Remove Permission from variable, check if specific Permission is existed inside variable
+
+            #endregion
+
+            #region 5.Create an enum called "Colors" with the basic colors (Red, Green, Blue) as its members. Write a C# program that takes a color name as input from the user and displays a message indicating whether the input color is a primary color or not.
+
+            #endregion
+
+            #region 6.Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+
+            #endregion
+
+            #region 7.Create a struct called "Person" with properties "Name" and "Age". Write a C# program that takes details of 3 persons as input from the user and displays the name and age of the oldest person.
+
+            #endregion
+
+
 
         }
     }
